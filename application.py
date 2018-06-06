834631df22b65f2f09cfde0c7e6485f3d6736e98
Bojan Stefanovic <bojan@formathletica.com>
2018-06-06 12:01:26 -0700

    Updated following tutorial "medium.com @miloharper a-beginner-s-guide-to-creating-your-first-python-website-using-flask-aws-ec2-elastic-beanstalk-6a82b9be25e0"

--- application.py
+++ application.py
@@ -3,26 +3,30 @@ from flask_cors import CORS
 from companyupdater import CompanyUpdater
 
 # Initialize the Flask app.
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+application = Flask(__name__)
+application.debug = True
+CORS(application, resources={r"/*": {"origins": "*"}})
 
 # Update interval is 5 minutes.
 # 5 minutes to seconds is 5 * 60 = 300.
 companyUpdater = CompanyUpdater(interval=300)
 
 # Sanity check.
-@app.route('/')
+@application.route('/')
 def index():
     return 'We are live.'
 
 # Add error handling.
-@app.route('/companies', methods=['GET'])
+@application.route('/companies', methods=['GET'])
 def companies():
     companies = companyUpdater.companies
     return jsonify({'data': companies})
 
 # Add error handling.
-@app.route('/symbol/<string:symbol>', methods=['GET'])
+@application.route('/symbol/<string:symbol>', methods=['GET'])
 def symbol(symbol):
     company = next(company for company in companyUpdater.companies if company['symbol'] == symbol)
     return jsonify({'data': company})
+
+if __name__ == '__main__':
+    application.run()
