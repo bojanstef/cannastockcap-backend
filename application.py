8515af7e3eadaff07ec8707c178f738089be2a7e
Bojan Stefanovic <bojan@formathletica.com>
2018-06-06 12:17:09 -0700

    Added comments for todos

--- application.py
+++ application.py
@@ -5,6 +5,8 @@ from companyupdater import CompanyUpdater
 # Initialize the Flask app.
 application = Flask(__name__)
 application.debug = True
+
+# Only allow requests from React app.
 CORS(application, resources={r"/*": {"origins": "*"}})
 
 # Update interval is 5 minutes.
@@ -16,13 +18,13 @@ companyUpdater = CompanyUpdater(interval=300)
 def index():
     return 'We are live.'
 
-# Add error handling.
+# Add error handling and 404.
 @application.route('/companies', methods=['GET'])
 def companies():
     companies = companyUpdater.companies
     return jsonify({'data': companies})
 
-# Add error handling.
+# Add error handling and 404.
 @application.route('/symbol/<string:symbol>', methods=['GET'])
 def symbol(symbol):
     company = next(company for company in companyUpdater.companies if company['symbol'] == symbol)
